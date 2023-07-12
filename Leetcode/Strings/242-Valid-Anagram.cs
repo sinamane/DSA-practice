1b1fa19a9bb5f2c9525c393064ef17424da85521
Amanda Sinamane <77048645+sinamane@users.noreply.github.com>
2023-07-12 13:05:37 +0200

    Problem 242 updated

--- Leetcode/Strings/242-Valid-Anagram.cs
+++ Leetcode/Strings/242-Valid-Anagram.cs
@@ -1,20 +1,24 @@
 /*
 Problem Statement:
-https://leetcode.com/problems/valid-palindrome/
+https://leetcode.com/problems/valid-anagram/
 
 */
 
 /*
 Approach 1:
-Remove all non-alphanumeric charcters and convert the string to lower case.
-Use two pointers moving toward the middle to each if each letters match.
+Convert both stings to lists.
+Remove every letter in the first list from the second list. 
+If a letter in the first list does not exist in the second list, return false.
+However, if all the letters are removed form the second list, return true.
 
 Runtime: 154 ms, faster than 7.29% of C# online submissions for Valid Anagram.
 Memory Usage: 40.9 MB, less than 24.75% of C# online submissions for Valid Anagram.
 
 Complexity Analysis:
 Time complextity:
+O(n), we loop through each character of the first string once.
 Space complexity:
+O(2n), 
 */
 
 using System.Collections.Generic;
@@ -39,15 +43,20 @@ public class Solution {
 
 /*
 Approach 2:
-Remove all non-alphanumeric charcters and convert the string to lower case.
-Use two pointers moving toward the middle to each if each letters match.
+Create an integer array of size 26, representing all letters of the alphabet.
+Using a forloop increase the value of the integer array using an index determined by finding ascii
+value of a character in the first string. We decrese the same valuse based on the second string.
+Use a foreach loop to ensure that all values in the integer array equal to zero. 
+If not false, otherwise return true. 
 
 Runtime: 80 ms, faster than 77.29% of C# online submissions for Valid Anagram.
 Memory Usage: 39.7 MB, less than 84.71% of C# online submissions for Valid Anagram.
 
 Complexity Analysis:
 Time complextity:
+O(n), we loop through each character of both strings once.
 Space complexity:
+O(1), because the size of the integer array is constant
 */
 
 public class Solution {
